d38ea2b6a4a00e861950c9b1e1e450ce58f2f2b1
darjaorlova <darja.orlova@chi.lv>
2023-05-05 12:35:06 +0300

    CH3: Refactor to use AnimatedBuilder with ChangeNotifier

--- Chapter03/candy_store/lib/cart_page.dart
+++ Chapter03/candy_store/lib/cart_page.dart
@@ -16,80 +16,69 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
   @override
-  void initState() {
-    super.initState();
-    widget.cartNotifier.addListener(_updateCart);
-  }
-
-  @override
-  void dispose() {
-    widget.cartNotifier.removeListener(_updateCart);
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cart'),
       ),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 60),
-            child: ListView.builder(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              itemCount: widget.cartNotifier.items.length,
-              itemBuilder: (context, index) {
-                final item = widget.cartNotifier.items[index];
-                return CartListItemView(
-                  item: item,
-                  cartNotifier: widget.cartNotifier,
-                );
-              },
-            ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 12,
+      body: AnimatedBuilder(
+        animation: widget.cartNotifier,
+        builder: (context, _) {
+          return Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 60),
+                child: ListView.builder(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  itemCount: widget.cartNotifier.items.length,
+                  itemBuilder: (context, index) {
+                    final item = widget.cartNotifier.items[index];
+                    return CartListItemView(
+                      item: item,
+                      cartNotifier: widget.cartNotifier,
+                    );
+                  },
+                ),
               ),
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Total:',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
                   ),
-                  Text(
-                    '${widget.cartNotifier.totalPrice} €',
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
                   ),
-                ],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Total:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '${widget.cartNotifier.totalPrice} €',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
+            ],
+          );
+        },
       ),
     );
-  }
-
-  void _updateCart() {
-    setState(() {});
   }
 }
