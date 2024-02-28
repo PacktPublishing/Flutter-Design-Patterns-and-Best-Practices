65ca7a77d3d4a1d1256631ea07ea1849f6b9c5a8
darjaorlova <dariaorlova.bbapps@gmail.com>
2024-02-28 11:56:42 +0200

    CH4: Add starting code

---
+++ CH04/final/candy_store/lib/cart_list_item_view.dart
@@ -0,0 +1,95 @@
+import 'package:candy_store/cart_list_item.dart';
+import 'package:candy_store/cart_notifier_provider.dart';
+import 'package:flutter/material.dart';
+
+class CartListItemView extends StatelessWidget {
+  final CartListItem item;
+
+  const CartListItemView({
+    super.key,
+    required this.item,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final cartNotifier = CartProvider.of(context);
+
+    final product = item.product;
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: Image.asset(product.imageUrl),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Text(
+                      product.description,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () => cartNotifier.removeFromCart(item),
+                          icon: Icon(
+                            Icons.remove,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                        Text(
+                          '${item.quantity}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => cartNotifier.addToCart(item.product),
+                          icon: Icon(
+                            Icons.add,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text(
+            '${product.price * item.quantity}€',
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
