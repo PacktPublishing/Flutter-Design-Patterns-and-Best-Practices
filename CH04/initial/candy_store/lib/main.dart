0f6bfe89d07545db44921123c2aca39e69cc0f32
darjaorlova <dariaorlova.bbapps@gmail.com>
2024-02-28 11:56:42 +0200

    CH4: Add starting code

---
+++ CH04/initial/candy_store/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:candy_store/cart_notifier.dart';
+import 'package:candy_store/cart_notifier_provider.dart';
+import 'package:candy_store/main_page.dart';
+import 'package:flutter/material.dart';
+
+// At this point, all of the code is in the `lib` folder and we will structure it in Part 3
+void main() {
+  runApp(
+    CartProvider(
+      cartNotifier: CartNotifier(),
+      child: MaterialApp(
+        title: 'Candy shop',
+        theme: ThemeData(
+          primarySwatch: Colors.lime,
+        ),
+        home: const MainPage(),
+      ),
+    ),
+  );
+}
