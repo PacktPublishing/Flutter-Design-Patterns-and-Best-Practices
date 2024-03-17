eea52c456885aeda3ec0d46258384f16197376e0
esrakadah <87121319+esrakadah@users.noreply.github.com>
2024-03-17 18:42:45 +0300

    creating CH05

---
+++ CH05/initial/candy_store/lib/cart_list_item.dart
@@ -0,0 +1,11 @@
+import 'package:candy_store/product_list_item.dart';
+
+class CartListItem {
+  final ProductListItem product;
+  final int quantity;
+
+  CartListItem({
+    required this.product,
+    required this.quantity,
+  });
+}
