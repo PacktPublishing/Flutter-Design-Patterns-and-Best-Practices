9c60efdd5e72a4d56a429e57ae0894d51e615386
darjaorlova <dariaorlova.bbapps@gmail.com>
2024-04-15 17:43:31 +0300

    CH6: Connect `AppProductRepository` to the `ProductsPage` via `ProductsBloc`

--- CH06/final/candy_store/lib/main.dart
+++ CH06/final/candy_store/lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:candy_store/api_service.dart';
 import 'package:candy_store/cart_view_model.dart';
 import 'package:candy_store/cart_view_model_provider.dart';
+import 'package:candy_store/hive_service.dart';
 import 'package:candy_store/main_page.dart';
 import 'package:flutter/material.dart';
 
+final hiveService = HiveService();
+final apiService = ApiService();
+
 // At this point, all of the code is in the `lib` folder and we will structure it in Part 3
-void main() {
+Future<void> main() async {
+  await hiveService.initializeHive();
   runApp(
     CartViewModelProvider(
       cartViewModel: CartViewModel(),
