c525fa0cfbcbfcb5214d0c8d91d6f103fb53a5b5
darjaorlova <dariaorlova.bbapps@gmail.com>
2024-04-15 16:35:33 +0300

    CH6: Remove redundant methods from the `ProductRepository`

--- CH06/final/candy_store/lib/app_product_repository.dart
+++ CH06/final/candy_store/lib/app_product_repository.dart
@@ -3,24 +3,10 @@ import 'package:candy_store/product_repository.dart';
 
 class AppProductRepository implements ProductRepository {
   @override
-  Future<List<Product>> fetchProduct() {
+  Future<List<Product>> fetchProducts() {
     // Fetch candies from the data source (e.g., local database or API)
     // Transform the raw data if necessary
     // Return the list of candies
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Product> fetchProductById(int id) {
-    // Fetch a specific Product by ID
-    // Transform the raw data if necessary
-    // Return the Product object
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> updateProduct(Product product) {
-    // Update product data in the data source
     throw UnimplementedError();
   }
 }
