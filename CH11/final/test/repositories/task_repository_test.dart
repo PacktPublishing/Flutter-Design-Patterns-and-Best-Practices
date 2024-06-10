e47b7b92b652d5728a73dcdd2186e07c1ff5dab3
Esra Kadah <87121319+esrakadah@users.noreply.github.com>
2024-06-10 04:31:51 +0200

    CH11: Create mocks for TaskService

---
+++ CH11/final/test/repositories/task_repository_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:ch11_testing/repositories/task_repository.dart';
+import 'package:ch11_testing/services/task_service.dart';
+
+@GenerateMocks([TaskService])
+import 'task_repository_test.mocks.dart';
+
+void main() {
+  // Test cases will be added here
+}
