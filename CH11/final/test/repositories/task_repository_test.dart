7e13c213e817461a03dd064b8ccd3e080c62f2b4
Esra Kadah <87121319+esrakadah@users.noreply.github.com>
2024-06-10 04:43:10 +0200

    CH11: Add repository test with mocked services

--- CH11/final/test/repositories/task_repository_test.dart
+++ CH11/final/test/repositories/task_repository_test.dart
@@ -1,12 +1,34 @@
+// test/repositories/task_repository_test.dart
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:ch11_testing/repositories/task_repository.dart';
 import 'package:ch11_testing/services/task_service.dart';
+import 'package:ch11_testing/models/task.dart';
 
+// Generate a MockTaskService class
 @GenerateMocks([TaskService])
 import 'task_repository_test.mocks.dart';
 
 void main() {
-  // Test cases will be added here
+  group('TaskRepository', () {
+    late TaskRepository repository;
+    late MockTaskService mockService;
+
+    setUp(() {
+      mockService = MockTaskService();
+      repository = TaskRepository(mockService);
+    });
+
+    test('should fetch tasks from the service', () async {
+      when(mockService.fetchTasks()).thenAnswer((_) async => [Task('Mock Task 1'), Task('Mock Task 2')]);
+
+      final tasks = await repository.getTasks();
+
+      expect(tasks.length, 2);
+      expect(tasks[0].title, 'Mock Task 1');
+      expect(tasks[1].title, 'Mock Task 2');
+      verify(mockService.fetchTasks()).called(1);
+    });
+  });
 }
