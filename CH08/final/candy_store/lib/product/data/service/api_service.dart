bb928b34ec0a27d374fadc36018d9b1fbda9fdba
darjaorlova <dariaorlova.bbapps@gmail.com>
2024-07-31 11:23:28 +0100

    CH8: Apply fixes from previous chapter

--- CH08/final/candy_store/lib/product/data/service/api_service.dart
+++ CH08/final/candy_store/lib/product/data/service/api_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import '../../domain/model/product.dart';
